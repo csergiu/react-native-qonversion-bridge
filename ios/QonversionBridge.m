00f0d789099659b763911dc048628c4773d3abb4
Sergiu Calborean <sergiu.calborean@gmail.com>
2020-05-12 21:24:26 +0300

    Added the Qonversion launch method

--- ios/QonversionBridge.m
+++ ios/QonversionBridge.m
@@ -1,13 +1,14 @@
 #import "QonversionBridge.h"
+#import "Qonversion.h"
 
 @implementation QonversionBridge
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(launch:(nonnull NSString *)apiKey customUserId:(NSString *)customUserId callback:(RCTResponseSenderBlock)callback)
 {
-    // TODO: Implement some actually useful functionality
-    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+    [Qonversion launchWithKey:apiKey userID:customUserId];
+    callback(@[[NSString stringWithFormat: @"apiKey: %@ customUserId: %@", apiKey, customUserId]]);
 }
 
 @end
